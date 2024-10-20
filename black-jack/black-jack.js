4cb5b3a21ee5ba0772e35c14a2380724e42cb5f6
mdchy <mdchy5000@gmail.com>
2024-10-20 16:34:15 +0600

    structured files and folders for different projects and started working on black-jack

---
+++ black-jack/black-jack.js
@@ -0,0 +1,12 @@
+let firstCard = 0
+let secondCard = 1
+
+let sum = firstCard + secondCard 
+
+if (sum < 21) {
+    console.log("do you want another card?")
+} else if (sum === 21) {
+    console.log("You've got blackjack!!")
+} else if (sum >) {
+    console.log("you've lost!")
+}
