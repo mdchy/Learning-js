778d13cf6ea93b1e081bcca9c1a9c72e5080ffc9
mdchy <mdchy5000@gmail.com>
2024-10-21 17:38:03 +0600

    further worked on blackjack, enhanced the function, used loop and array to process a lot of data. Linked project from index.html

--- black-jack/black-jack.js
+++ black-jack/black-jack.js
@@ -1,13 +1,26 @@
 let firstCard = 20
 let secondCard = 2
+let cards = [firstCard, secondCard]
 let isBlackJack = false
 let isAlive = true 
 let sum = firstCard + secondCard 
 let message = ""
+let messageEl = document.querySelector("#message-el")
+let sumEl = document.querySelector("#sum-el")
+let cardsEl = document.querySelector("#cards-el")
 
 function startGame () {
+    renderGame()
+}
+
+function renderGame () {
+
+    for (let i = 0; i <= cards.length; i++) {
+        cardsEl.textContent = cards[i] + " "
+    }
+
     if (sum < 21) {
-        console.log("do you want another card?")
+        message = ("do you want another card?")
     } else if (sum === 21) {
         message = "You've got blackjack!!"
         isBlaskJack = true
@@ -15,7 +28,13 @@ function startGame () {
         message = "you've lost!"
         isAlive = false 
     }
-    console.log(message)
-    console.log(isAlive)
+    messageEl.textContent = " " + message 
+    sumEl.textContent = " " + sum;
+}
+
+function newCard () {
+    let freshCard = 5
+    cards.push(freshCard)
+    renderGame()
 }
 
