97f75e4dd640c26155933d4c03cc82b7c0887611
mdchy <mdchy5000@gmail.com>
2024-10-22 15:47:52 +0600

    testing conflicting push

--- black-jack/black-jack.js
+++ black-jack/black-jack.js
@@ -5,25 +5,25 @@ let isBlackJack = false
 let isAlive = true 
 let sum = firstCard + secondCard 
 let message = ""
-let messageEl = document.querySelector("#message-el")
-let sumEl = document.querySelector("#sum-el")
-let cardsEl = document.querySelector("#cards-el")
+let messageEl = document.getElementById("message-el")
+let sumEl = document.getElementById("sum-el")
+let cardsEl = document.getElementById("cards-el")
 
 function startGame () {
     renderGame()
 }
 
 function renderGame () {
-
-    for (let i = 0; i <= cards.length; i++) {
-        cardsEl.textContent = cards[i] + " "
+    cardsEl.textContent = " "
+    for (let i = 0; i < cards.length; i+= 1) {
+        cardsEl.textContent += cards[i] + " "
     }
 
     if (sum < 21) {
         message = ("do you want another card?")
     } else if (sum === 21) {
         message = "You've got blackjack!!"
-        isBlaskJack = true
+        isBlackJack = true
     } else {
         message = "you've lost!"
         isAlive = false 
@@ -34,6 +34,7 @@ function renderGame () {
 
 function newCard () {
     let freshCard = 5
+    sum += freshCard
     cards.push(freshCard)
     renderGame()
 }
